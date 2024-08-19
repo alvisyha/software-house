75afbb5acf616616c6a18c6769088bf40910e17a
alvisyha <alvisyha@gmail.com>
2024-08-19 10:53:51 +0700

    add about and service

---
+++ assets/js/script.js
@@ -0,0 +1,25 @@
+const swiper = new Swiper(".mySwiper", {
+  slidesPerView: 1,
+  spaceBetween: 10,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  breakpoints: {
+    640: {
+      slidesPerView: 2,
+      spaceBetween: 10,
+    },
+    768: {
+      slidesPerView: 3,
+      spaceBetween: 15,
+    },
+    1024: {
+      slidesPerView: 4,
+      spaceBetween: 20,
+    },
+  },
+  autoplay: {
+    delay: 2000,
+  },
+});
